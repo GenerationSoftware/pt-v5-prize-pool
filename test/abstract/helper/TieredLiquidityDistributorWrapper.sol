e0b1b3f16c166c72ad19ab6b869af2649d5be7b3
Dylan DesRosier <dylan@pooltogether.com>
2023-06-29 18:27:47 +0100

    Remove duplicate method

--- test/abstract/helper/TieredLiquidityDistributorWrapper.sol
+++ test/abstract/helper/TieredLiquidityDistributorWrapper.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.17;
 
 import "forge-std/console2.sol";
 
-import { TieredLiquidityDistributor, Tier, fromUD34x4toUD60x18 } from "src/abstract/TieredLiquidityDistributor.sol";
+import { TieredLiquidityDistributor, Tier, fromUD34x4toUD60x18, fromUD60x18 } from "src/abstract/TieredLiquidityDistributor.sol";
 
 contract TieredLiquidityDistributorWrapper is TieredLiquidityDistributor {
   constructor(
@@ -34,7 +34,16 @@ contract TieredLiquidityDistributorWrapper is TieredLiquidityDistributor {
   function remainingTierLiquidity(uint8 _tier) external view returns (uint112) {
     uint8 shares = _computeShares(_tier, numberOfTiers);
     Tier memory tier = _getTier(_tier, numberOfTiers);
-    return _remainingTierLiquidity(tier, shares);
+    return
+      uint112(
+        fromUD60x18(
+          _getTierRemainingLiquidity(
+            shares,
+            fromUD34x4toUD60x18(tier.prizeTokenPerShare),
+            fromUD34x4toUD60x18(prizeTokenPerShare)
+          )
+        )
+      );
   }
 
   function getTierLiquidityToReclaim(uint8 _nextNumberOfTiers) external view returns (uint256) {
