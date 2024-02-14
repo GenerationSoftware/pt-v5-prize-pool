9ebdc472c894fea51e577ccb15e2d3ddca0d3070
Brendan Asselstine <mail.asselstine@gmail.com>
2024-02-14 11:01:07 -0800

    Removed unused line from PrizePoolFactory

--- src/PrizePoolFactory.sol
+++ src/PrizePoolFactory.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import { IERC20, IERC4626 } from "openzeppelin/token/ERC20/extensions/ERC4626.sol";
-
 import { PrizePool, ConstructorParams } from "./PrizePool.sol";
 
 /**
