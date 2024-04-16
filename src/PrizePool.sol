178c87179800d489cc386b766b34e2be91a10fb8
Brendan Asselstine <mail.asselstine@gmail.com>
2024-04-16 16:21:37 -0700

    Removed console2

--- src/PrizePool.sol
+++ src/PrizePool.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import { console2 } from "forge-std/console2.sol";
-
 import { SafeCast } from "openzeppelin/utils/math/SafeCast.sol";
 import { IERC20 } from "openzeppelin/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
